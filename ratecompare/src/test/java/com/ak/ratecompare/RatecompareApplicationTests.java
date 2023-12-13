77346e4e3c2896b96751d53b4a361cad25a6708b
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2023-12-13 18:02:34 +0530

    Backend creation

---
+++ ratecompare/src/test/java/com/ak/ratecompare/RatecompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.ratecompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatecompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
