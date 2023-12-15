139a445ca6ebcea2aa9c23b99328fc98fd4781bd
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2023-12-15 10:52:02 +0530

    Moved ApiClient to interfaces package

--- ratecompare/src/main/java/com/ak/ratecompare/exchangerate/interfaces/ExchangeRateApiClient.java
+++ ratecompare/src/main/java/com/ak/ratecompare/exchangerate/interfaces/ExchangeRateApiClient.java
@@ -1,4 +1,4 @@
-package com.ak.ratecompare.exchangerate;
+package com.ak.ratecompare.exchangerate.interfaces;
 
 import com.ak.ratecompare.exchangerate.model.ExchangeRate;
 
--- ratecompare/src/main/java/com/ak/ratecompare/exchangerate/service/ExchangeRateClientManagerService.java
+++ ratecompare/src/main/java/com/ak/ratecompare/exchangerate/service/ExchangeRateClientManagerService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ak.ratecompare.exchangerate.ExchangeRateApiClient;
+import com.ak.ratecompare.exchangerate.interfaces.ExchangeRateApiClient;
 
 @Service
 public class ExchangeRateClientManagerService {
