4f11ded806a39b1a9fccb6ef1a608969cc58f2b3
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2023-12-15 11:43:42 +0530

    Updated to reflect Provider changes

--- ratecompare/src/main/java/com/ak/ratecompare/exchangerate/dataloader/ExchangeRateDataLoader.java
+++ ratecompare/src/main/java/com/ak/ratecompare/exchangerate/dataloader/ExchangeRateDataLoader.java
@@ -4,30 +4,41 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.ak.ratecompare.exchangerate.model.ExchangeRate;
+import com.ak.ratecompare.exchangerate.model.Provider;
 import com.ak.ratecompare.exchangerate.repository.ExchangeRateRepository;
+import com.ak.ratecompare.exchangerate.repository.ProviderRepository;
 
 @Component
+@Order(2)
 public class ExchangeRateDataLoader implements CommandLineRunner {
-	
-	private final ExchangeRateRepository exchangeRateRepository;
 
-	public ExchangeRateDataLoader(ExchangeRateRepository exchangeRateRepository) {
+	private final ExchangeRateRepository exchangeRateRepository;
+	private final ProviderRepository providerRepository;
+
+	public ExchangeRateDataLoader(ExchangeRateRepository exchangeRateRepository,
+			ProviderRepository providerRepository) {
 		this.exchangeRateRepository = exchangeRateRepository;
+		this.providerRepository = providerRepository;
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		ExchangeRate rate1 = new ExchangeRate( "USD", "EUR", "Wise", new BigDecimal("0.85"), LocalDateTime.now());
-        ExchangeRate rate2 = new ExchangeRate("USD", "EUR", "Revolut", new BigDecimal("0.84"), LocalDateTime.now());
-        
 
-        exchangeRateRepository.save(rate1);
-        exchangeRateRepository.save(rate2);
+		Provider wiseProvider = providerRepository.findById("Wise")
+				.orElseThrow(() -> new RuntimeException("Provider not Found!"));
+		Provider revolutProvider = providerRepository.findById("Revolut")
+				.orElseThrow(() -> new RuntimeException("Provider not Found!"));
+
+		ExchangeRate rate1 = new ExchangeRate("USD", "EUR", wiseProvider, new BigDecimal("0.85"), LocalDateTime.now());
+		ExchangeRate rate2 = new ExchangeRate("USD", "EUR", revolutProvider, new BigDecimal("0.84"),
+				LocalDateTime.now());
+
+		exchangeRateRepository.save(rate1);
+		exchangeRateRepository.save(rate2);
 	}
-	
-	
 
 }
