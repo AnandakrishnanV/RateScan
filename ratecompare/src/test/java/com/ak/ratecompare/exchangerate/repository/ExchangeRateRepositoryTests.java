e287d83325dbbc71d9882560b54c73a4a48e454a
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2023-12-15 11:57:14 +0530

    Updated Tests

--- ratecompare/src/test/java/com/ak/ratecompare/exchangerate/repository/ExchangeRateRepositoryTests.java
+++ ratecompare/src/test/java/com/ak/ratecompare/exchangerate/repository/ExchangeRateRepositoryTests.java
@@ -2,6 +2,8 @@ package com.ak.ratecompare.exchangerate.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,12 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ak.ratecompare.exchangerate.model.ExchangeRate;
+import com.ak.ratecompare.exchangerate.model.Provider;
 
 @SpringBootTest
 public class ExchangeRateRepositoryTests {
 
 	@Autowired
 	private ExchangeRateRepository exchangeRateRepository;
+	
+	@Autowired
+	private ProviderRepository providerRepository;
 
 	@Test
 	public void whenFindByCurrencyPair_thenReturnRates() {
@@ -27,9 +33,24 @@ public class ExchangeRateRepositoryTests {
 	@Test
 	public void whenFindByCurrencyPairAndProvider_thenReturnRates() {
 		
-		Optional<ExchangeRate> rateOptional = exchangeRateRepository.findBySourceCurrencyAndTargetCurrencyAndProviderOrderByTimestampDesc("USD", "EUR", "Wise");
+		Provider wiseProvider = providerRepository.findById("Wise")
+				.orElseThrow(() -> new RuntimeException("Provider not Found!"));
+		
+		Optional<ExchangeRate> rateOptional = exchangeRateRepository.findTopBySourceCurrencyAndTargetCurrencyAndProviderNameOrderByTimestampDesc("USD", "EUR", wiseProvider.getName());
 	
 		assertThat(rateOptional).isPresent();
 	    assertThat(rateOptional.get().getProvider()).isEqualTo("Wise");
+	}
+	
+	@Test
+	public void whenFindByCurrencyPairAndProvider_thenReturnRatesWithTestProvider() {
+		
+		Provider testProvider = new Provider("TestProvider", "https://api.test.com", "testApiKey", null, null);
+		ExchangeRate testRate = new ExchangeRate("USD", "EUR", testProvider, new BigDecimal("0.95"), LocalDateTime.now());
+		
+		Optional<ExchangeRate> rateOptional = exchangeRateRepository.findTopBySourceCurrencyAndTargetCurrencyAndProviderNameOrderByTimestampDesc("USD", "EUR", "TestProvider");
+	
+		assertThat(rateOptional).isPresent();
+	    assertThat(rateOptional.get().getProvider()).isEqualTo("TestProvider");
 	}
 }
