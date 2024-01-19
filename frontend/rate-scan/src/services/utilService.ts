2a181b553c0bd2f465a637cc130a01ffd79a1994
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2024-01-19 21:11:59 +0530

    added util for seconds ago

--- frontend/rate-scan/src/services/utilService.ts
+++ frontend/rate-scan/src/services/utilService.ts
@@ -1,9 +1,18 @@
 import { formatDistance } from 'date-fns';
 
 export const timeAgo = (dateString: string | number | Date) => {
+
+    const date = new Date(dateString)
+    const now = new Date()
+    const secondsDiff = (now.getTime() - date.getTime()) / 1000
+
+    if(secondsDiff < 60) {
+        return `${Math.round(secondsDiff)} seconds ago`
+    }
+
     return formatDistance(
-        new Date(dateString),
-        new Date(),
+        date,
+        now,
         { addSuffix: true}
     )
 }
