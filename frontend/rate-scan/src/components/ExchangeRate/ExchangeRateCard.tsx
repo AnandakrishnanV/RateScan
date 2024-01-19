71984617d6e64b603202bba43e38e55e4771e149
AnandakrishnanV <iamanandakrishnanv@gmail.com>
2024-01-19 19:25:31 +0530

    updated card, styles

--- frontend/rate-scan/src/components/ExchangeRate/ExchangeRateCard.tsx
+++ frontend/rate-scan/src/components/ExchangeRate/ExchangeRateCard.tsx
@@ -1,30 +1,52 @@
 import './ExchangeRateCard.scss'
-import { timeAgo } from '../../services/utilService';
+import providerLogos from '../../services/providerLogos'
+import { timeAgo } from '../../services/utilService'
 
 const ExchangeRateCard = ({ rateData }) => {
-    return (
-      <div className="card exchange-rate-card mb-3">
-        <div className="card-body">
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="rate-score">
+  const logoUrl = providerLogos[rateData.providerName]
+
+  console.log(logoUrl)
+
+  return (
+    <div className="card exchange-rate-card mb-3">
+      <div className="card-body">
+        <div className="d-flex justify-content-between align-items-center">
+          {/* <div className="rate-score">
               <span className="badge bg-success">8</span>
-              <span className="provider-name">Wise</span>
-            </div>
-            <div className="rate-details">
-              {/* <p>Transfer time: {rateData.timestamp}</p> */}
-              {/* <p>Fee & rates: 10</p> */}
-              <p>Exchange rate: {rateData.rate} {rateData.targetCurrency}</p>
-              <p>Rate Obtained: {timeAgo(rateData.timestamp)}</p>
-              <p className="text-success">{rateData.sourceCurrency}</p>
-            </div>
-            <div className="recipient-gets">
-              <p>{rateData.amount} {rateData.targetCurrency}</p>
-            </div>
-            <a href={rateData.providerName} className="btn btn-primary">Go to {rateData.providerName}</a>
+              <span className="provider-name">{rateData.providerName}</span>
+            </div> */}
+          <div className="rate-logo">
+            <img src={logoUrl} alt={`${rateData.providerName} logo`} />
           </div>
+
+          <div className="rate-details">
+            {/* <p>Transfer time: {rateData.timestamp}</p> */}
+            {/* <p>Fee & rates: 10</p> */}
+            <div className='exchange-rate'>
+              <span className="exchange-rate-label">Exchange rate: </span>
+              <span className="exchange-rate-value">
+                {rateData.rate} {rateData.targetCurrency}
+              </span>
+            </div>
+            <div className='rate-obtained'>
+              <span className="rate-obtained-label">
+                Rate Obtained {timeAgo(rateData.timestamp)}
+              </span>
+            </div>
+            {/* <p className="text-success">' '</p> */}
+          </div>
+          <div className="recipient-gets">
+            <p>
+              {rateData.amount} {rateData.targetCurrency}
+            </p>
+          </div>
+          <a href={rateData.providerName} className="btn btn-primary">
+            Go to {rateData.providerName}
+          </a>
         </div>
       </div>
-    );
-  };
+    </div>
+  )
+}
 
-  export default ExchangeRateCard;
+export default ExchangeRateCard
